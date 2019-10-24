80ccf71b4a35cd40917ce8724689eb1e46264716
Dmitry Protkin <infdma@mail.ru>
2019-10-24 13:57:06 +0300

    @Author Dmitry Protkin infdma@mail.ru

---
+++ src/ru/company/test/handh/HandH.java
@@ -0,0 +1,22 @@
+package ru.company.test.handh;
+
+public class HandH {
+
+    public static int max = 0;
+    public static int[] array = {3, 7, 12, 28, 8};
+
+    public static void main(String[] args) {
+        findMax(array);
+    }
+
+    public static int findMax(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > array[max]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+}
+
+
